ba358b60e0933b2112330578b40191f0bade3dee
ricard1997 <ricar.d1997@hotmail.com>
2020-09-30 22:14:50 -0500

    restauracion vesion anterior

--- script.js
+++ script.js
@@ -1,150 +1,94 @@
-const slides = document.querySelector(".slider").children;
-const prev = document.querySelector(".prev");
-const next = document.querySelector(".next");
-const indicator = document.querySelector(".indicator");
-let index = 0;
 
 
-prev.addEventListener("click", function() {
-  prevSlide();
-  updateCircleIndicator();
-  resetTimer();
-})
+ const slides=document.querySelector(".slider").children;
+ const prev=document.querySelector(".prev");
+ const next=document.querySelector(".next");
+ const indicator=document.querySelector(".indicator");
+ let index=0;
 
-next.addEventListener("click", function() {
-  nextSlide();
-  updateCircleIndicator();
-  resetTimer();
 
-})
+   prev.addEventListener("click",function(){
+       prevSlide();
+       updateCircleIndicator();
+       resetTimer();
+   })
 
-// create circle indicators
-function circleIndicator() {
-  for (let i = 0; i < slides.length; i++) {
-    const div = document.createElement("div");
-    div.innerHTML = i + 1;
-    div.setAttribute("onclick", "indicateSlide(this)")
-    div.id = i;
-    if (i == 0) {
-      div.className = "active";
+   next.addEventListener("click",function(){
+      nextSlide();
+      updateCircleIndicator();
+      resetTimer();
+
+   })
+
+   // create circle indicators
+    function circleIndicator(){
+        for(let i=0; i< slides.length; i++){
+        	const div=document.createElement("div");
+        	      div.innerHTML=i+1;
+                div.setAttribute("onclick","indicateSlide(this)")
+                div.id=i;
+                if(i==0){
+                	div.className="active";
+                }
+               indicator.appendChild(div);
+        }
     }
-    indicator.appendChild(div);
-  }
-}
-circleIndicator();
+    circleIndicator();
 
-function indicateSlide(element) {
-  index = element.id;
-  changeSlide();
-  updateCircleIndicator();
-  resetTimer();
-}
-
-function updateCircleIndicator() {
-  for (let i = 0; i < indicator.children.length; i++) {
-    indicator.children[i].classList.remove("active");
-  }
-  indicator.children[index].classList.add("active");
-}
-
-function prevSlide() {
-  if (index == 0) {
-    index = slides.length - 1;
-  } else {
-    index--;
-  }
-  changeSlide();
-}
-
-function nextSlide() {
-  if (index == slides.length - 1) {
-    index = 0;
-  } else {
-    index++;
-  }
-  changeSlide();
-}
-
-function changeSlide() {
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].classList.remove("active");
-  }
-
-  slides[index].classList.add("active");
-}
-
-function resetTimer() {
-  // when click to indicator or controls button
-  // stop timer
-  clearInterval(timer);
-  // then started again timer
-  timer = setInterval(autoPlay, 4000);
-}
-
-
-function autoPlay() {
-  nextSlide();
-  updateCircleIndicator();
-}
-
-let timer = setInterval(autoPlay, 4000);
-
-
-
-
-const menuItems = document.querySelectorAll('.menu a[href^="#"]');
-
-menuItems.forEach(item => {
-  item.addEventListener('click', scrollToIdOnClick);
-})
-
-function getScrollTopByHref(element) {
-  const id = element.getAttribute('href');
-  return document.querySelector(id).offsetTop;
-}
-
-function scrollToIdOnClick(event) {
-  event.preventDefault();
-  const to = getScrollTopByHref(event.target) - 80;
-  scrollToPosition(to);
-}
-
-function scrollToPosition(to) {
-  // window.scroll({
-  //   top: to,
-  //   behavior: "smooth",
-  // });
-  smoothScrollTo(0, to);
-}
-
-/**
- * Smooth scroll animation
- * @param {int} endX: destination x coordinate
- * @param {int} endY: destination y coordinate
- * @param {int} duration: animation duration in ms
- */
-function smoothScrollTo(endX, endY, duration) {
-  const startX = window.scrollX || window.pageXOffset;
-  const startY = window.scrollY || window.pageYOffset;
-  const distanceX = endX - startX;
-  const distanceY = endY - startY;
-  const startTime = new Date().getTime();
-
-  duration = typeof duration !== 'undefined' ? duration : 500;
-
-  // Easing function
-  const easeInOutQuart = (time, from, distance, duration) => {
-    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
-    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
-  };
-
-  const timer = setInterval(() => {
-    const time = new Date().getTime() - startTime;
-    const newX = easeInOutQuart(time, startX, distanceX, duration);
-    const newY = easeInOutQuart(time, startY, distanceY, duration);
-    if (time >= duration) {
-      clearInterval(timer);
+    function indicateSlide(element){
+         index=element.id;
+         changeSlide();
+         updateCircleIndicator();
+         resetTimer();
     }
-    window.scroll(newX, newY);
-  }, 100 / 60); // 60 fps
-};
+
+    function updateCircleIndicator(){
+    	for(let i=0; i<indicator.children.length; i++){
+    		indicator.children[i].classList.remove("active");
+    	}
+    	indicator.children[index].classList.add("active");
+    }
+
+   function prevSlide(){
+   	 if(index==0){
+   	 	index=slides.length-1;
+   	 }
+   	 else{
+   	 	index--;
+   	 }
+   	 changeSlide();
+   }
+
+   function nextSlide(){
+      if(index==slides.length-1){
+      	index=0;
+      }
+      else{
+      	index++;
+      }
+      changeSlide();
+   }
+
+   function changeSlide(){
+   	       for(let i=0; i<slides.length; i++){
+   	       	 slides[i].classList.remove("active");
+   	       }
+
+       slides[index].classList.add("active");
+   }
+
+   function resetTimer(){
+   	  // when click to indicator or controls button
+   	  // stop timer
+   	  clearInterval(timer);
+   	  // then started again timer
+   	  timer=setInterval(autoPlay,4000);
+   }
+
+
+  function autoPlay(){
+      nextSlide();
+      updateCircleIndicator();
+  }
+
+  let timer=setInterval(autoPlay,4000);
