02ef0b30db2ed7f05bb82bdc0931d1a44d54b34a
ricard1997 <ricar.d1997@hotmail.com>
2020-10-03 13:36:35 -0500

    negrita en en navbar

--- evento.js
+++ evento.js
@@ -1,7 +1,6 @@
 window.addEventListener('scroll', function() {
   let animacion = document.getElementById('animado');
   let posicionObj1 = animacion.getBoundingClientRect().top;
-  console.log(posicionObj1);
   let tamañoDePantalla = window.innerHeight / 3;
 
   if (posicionObj1 < tamañoDePantalla) {
