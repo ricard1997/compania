14d542488a5306e34ea68f49e8b97c1c098d8a45
ricard1997 <ricar.d1997@hotmail.com>
2020-10-02 20:21:31 -0500

    quito jss y aumento preloader

--- script.js
+++ script.js
@@ -102,3 +102,9 @@ menuBtn.addEventListener('click', () => {
     menuOpen = false;
   }
 });
+
+window.addEventListener("load",() =>  {
+  const preloader = document.querySelector('.preload');
+
+  preloader.classList.add('preload-finish');
+});
